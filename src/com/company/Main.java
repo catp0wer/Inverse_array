42071beb6e5993f21462bc6558778d6bd4bd4421
catp0wer <catp0wer@mail.ru>
2017-11-22 22:50:50 +0200

    changed the names of variables to be self-explanatory

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,20 @@ package com.company;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        float[] my_array = {2f, 5.6f, 6f, 7f};
-        System.out.println(Arrays.toString(my_array));
-        float[] inversed_array = new float[4];
-        int j=0;
-        for (int i = my_array.length - 1; i >= 0; i--){
-            inversed_array[j] = my_array[i];
-            System.out.println(inversed_array[j]);
-                j++;
-            }
-       System.out.println(Arrays.toString(inversed_array));
-        }
 
+    public static void main(String[] args) {
+
+        float[] initial_array = {2f, 5.6f, 6f, 7f};
+        System.out.println(Arrays.toString(initial_array));
+        float[] inversed_array = new float[4];
+        int index_inversed_array = 0;
+
+        for (int index_initial_Array = initial_array.length - 1; index_initial_Array >= 0; index_initial_Array--){
+            inversed_array[index_inversed_array] = initial_array[index_initial_Array];
+            System.out.println(inversed_array[index_inversed_array]);
+            index_inversed_array ++;
+        }
+        System.out.println(Arrays.toString(inversed_array));
     }
+
+}
